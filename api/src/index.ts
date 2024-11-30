3df3d9f1d43b0efd1be908afdb4c03a633ccd6b5
Algo53 <varunjangra4953@gmail.com>
2024-11-30 16:08:41 +0530

    Initial Commit

--- api/src/index.ts
+++ api/src/index.ts
@@ -13,7 +13,7 @@ const app = express();
 ConnectToMongoDB();
 
 app.use(cors({
-  origin: [process.env.CLIENT_URL ? process.env.CLIENT_URL : "https://hm-frontend-six.vercel.app/" , "https://hm-frontend-six.vercel.app"],
+  origin: ["https://hm-frontend-six.vercel.app"],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }))
