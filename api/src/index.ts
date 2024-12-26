6e354db3f8fbed86d07bd2e13990d0d6d65d5150
Algo53 <varunjangra4953@gmail.com>
2024-12-26 19:44:59 +0530

    Update Client Url

--- api/src/index.ts
+++ api/src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 ConnectToMongoDB();
 
 app.use(cors({
-  origin: [process.env.CLIENT_URL || "https://hm-frontend-six.vercel.app"],
+  origin: [process.env.CLIENT_URL || "https://hospital-management-neon.vercel.app"],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }))
