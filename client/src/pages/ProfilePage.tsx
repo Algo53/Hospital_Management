c8663d98c0b6257a18ba4f33e211f1597f4af003
Algo53 <varunjangra4953@gmail.com>
2025-01-25 04:43:42 +0530

    Update Profile Page

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -39,13 +39,13 @@ function ProfilePage() {
 
   return (
     <div className='flex flex-col flex-grow w-full h-full gap-3'>
-      <div className='flex text-xl font-bold'>Manage Your Personal Informations</div>
+      <div className='flex text-md xs:text-lg md:text-xl font-bold'>Manage Your Personal Informations</div>
       <div className='flex flex-grow flex-col gap-3 p-3 w-full h-full border-2 border-black/50 rounded-lg bg-white shadow-xl hover:shadow-2xl overflow-y-scroll hide-scrollbar'>
-        <div className="flex w-full justify-between">
-          <div className='flex md:text-xl text-lg font-semibold'>General Information</div>
-          <div className="flex gap-2 items-center">
-            <div className='flex md:text-2xl sm:text-xl text-lg font-semibold'>Role</div>
-            <div className="flex sm:text-lg text-md font-medium p-1 bg-black/5 border-1 border-black rounded-lg shadow-inner">{userRole}</div>
+        <div className="flex w-full justify-between items-center">
+          <div className='flex text-sm md:text-xl text-lg font-semibold'>General Information</div>
+          <div className="flex xs:gap-2 gap-1 items-center">
+            <div className='flex md:text-2xl xs:text-xl text-md font-semibold'>Role</div>
+            <div className="flex xs:text-lg text-sm font-medium p-1 bg-black/5 border-1 border-black rounded-lg shadow-inner">{userRole}</div>
           </div>
         </div>
         <div className='flex w-full h-[2px] bg-slate-400'></div>
@@ -58,7 +58,7 @@ function ProfilePage() {
             (
               <>
                 <div className='flex items-center'>
-                  <div className="flex w-1/2 text-xl font-semibold">Photo</div>
+                  <div className="flex w-1/2 xs:text-xl text-md font-semibold">Photo</div>
                   <div className="flex w-1/2">
                     {
                       userDetails?.photo ?
@@ -74,16 +74,16 @@ function ProfilePage() {
                       <>
                         <div key={index} className="flex flex-col md:flex-row gap-3 md:gap-0 items-center">
                           <div className="flex w-full md:w-1/2 justify-between">
-                            <div className="flex w-1/2 text-xl font-semibold">{PersonalVariables[index]}</div>
-                            <div className="flex w-1/2 font-normal text-lg">
+                            <div className="flex w-1/2 xs:text-xl text-md font-semibold">{PersonalVariables[index]}</div>
+                            <div className="flex w-1/2 font-normal xs:text-lg text-md">
                               {userDetails?.[PersonalVariables[index + 1] as keyof typeof userInfo]}
                             </div>
                           </div>
                           <div className='md:hidden flex w-full h-[1px] bg-slate-400'></div>
                           {index + 2 < PersonalVariables.length && index + 3 < PersonalVariables.length && (
                             <div className="flex w-full md:w-1/2 justify-between">
-                              <div className="flex w-1/2 text-xl font-semibold">{PersonalVariables[index + 2]}</div>
-                              <div className="flex w-1/2 font-normal text-lg">
+                              <div className="flex w-1/2 xs:text-xl text-md font-semibold">{PersonalVariables[index + 2]}</div>
+                              <div className="flex w-1/2 font-normal xs:text-lg text-md">
                                 {userDetails?.[PersonalVariables[index + 3] as keyof typeof userInfo]}
                               </div>
                             </div>
@@ -101,7 +101,7 @@ function ProfilePage() {
           userRole === 'Admin' ?
             (
               <div className="flex w-full items-center">
-                <div className="flex w-1/2 text-xl font-semibold">Organisations</div>
+                <div className="flex w-1/2 xs:text-xl text-md font-semibold">Organisations</div>
                 <div className="flex w-1/2 gap-1 text-lg font-medium">
                   {
                     data?.companyName.map((item: string, index: number) => (
@@ -115,15 +115,15 @@ function ProfilePage() {
               (
                 <div className="flex flex-col md:flex-row gap-3 justify-center">
                   <div className='flex w-full md:w-1/2 justify-between items-center'>
-                    <div className="flex w-1/2 text-xl font-semibold">Organisation</div>
-                    <div className="flex w-1/2 gap-1 text-lg font-medium">{data?.companyName}</div>
+                    <div className="flex w-1/2 xs:text-xl text-md font-semibold">Organisation</div>
+                    <div className="flex w-1/2 gap-1 xs:text-xl text-md font-medium">{data?.companyName}</div>
                   </div>
                   <div className='flex md:hidden w-full h-[1px] bg-slate-400'></div>
                   <div className='flex w-full md:w-1/2 justify-between items-center'>
-                    <div className="flex w-1/2 text-xl font-semibold">Department</div>
+                    <div className="flex w-1/2 xs:text-xl text-md font-semibold">Department</div>
                     <div className="flex w-1/2">
                       {
-                        data?.department && <div className="flex p-1 bg-black/10 rounded-md">{data?.department}</div>
+                        data?.department && <div className="flex p-1 bg-black/10 rounded-md xs:text-xl text-md">{data?.department}</div>
                       }
                     </div>
                   </div>
@@ -132,8 +132,8 @@ function ProfilePage() {
               :
               (
                 <div className='flex w-full items-center'>
-                  <div className="flex w-1/2 text-xl font-semibold">Organisation</div>
-                  <div className="flex w-1/2 gap-1 text-lg font-medium">{data?.companyName}</div>
+                  <div className="flex w-1/2 xs:text-xl text-md font-semibold">Organisation</div>
+                  <div className="flex w-1/2 gap-1 xs:text-xl text-md font-medium">{data?.companyName}</div>
                 </div>
               )
         }
@@ -143,18 +143,18 @@ function ProfilePage() {
               <div className='flex w-full h-[1px] bg-slate-400'></div>
               <div className="flex flex-col justify-center gap-3">
                 <div className='flex w-full justify-between items-center'>
-                  <div className="flex w-1/2 text-xl font-semibold">Degree</div>
+                  <div className="flex w-1/2 xs:text-xl text-md font-semibold">Degree</div>
                   <div className="flex flex-col md:flex-row w-1/2 gap-1">
                     {
                       data?.degree && data.degree.map((item: string, index: number) => (
-                        <div key={index} className="flex p-1 bg-black/10 rounded-md">{item}</div>
+                        <div key={index} className="flex p-1 bg-black/10 rounded-md whitespace-nowrap overflow-x-scroll hide-scrollbar">{item}</div>
                       ))
                     }
                   </div>
                 </div>
                 <div className='flex w-full h-[1px] bg-slate-400'></div>
                 <div className='flex w-full justify-between items-center'>
-                  <div className="flex w-1/2 text-xl font-semibold">Specialization</div>
+                  <div className="flex w-1/2 xs:text-xl text-md font-semibold">Specialization</div>
                   <div className="flex w-1/2">
                     {
                       data?.specialization && <div className="flex p-1 bg-black/10 rounded-md">{data?.specialization}</div>
